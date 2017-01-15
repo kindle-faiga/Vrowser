334ddbaa8805ad4ed0df3903299eb8cf9145e5b7
Syuma Toyama <Syuma Toyama>
2017-01-15 19:20:28 +0900

    no message

---
+++ Assets/Develop/Script/TutorialControl.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialControl : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] pages;
+    private int count = 0;
+    private bool isStart = false;
+
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!isStart)
+            {
+                isStart = true;
+                StartCoroutine(WaitForChange());
+            }
+        }
+    }
+
+    IEnumerator WaitForChange()
+    {
+        pages[count].SetActive(false);
+
+        if (count < 6)
+        {
+            ++count;
+        }
+        else
+        {
+            count = 1;
+        }
+
+        pages[count].SetActive(true);
+
+        yield return new WaitForSeconds(30.0f);
+
+        StartCoroutine(WaitForChange());
+    }
+}
---
+++ Assets/Develop/Script/TutorialManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialManager : MonoBehaviour
+{
+	[SerializeField]
+	private Sprite[] tutorialImages;
+	private Image mainImage;
+	private int count = 0;
+	private float easeTime = 0.1f;
+	private float waitRange = 5.0f;
+	private Vector3 defaultSize;
+
+	void Start()
+	{
+		mainImage = GetComponent<Image>();
+
+		mainImage.sprite = tutorialImages[0];
+
+		defaultSize = transform.localScale;
+
+		transform.localScale = Vector3.zero;
+
+		StartCoroutine(WaitForUpdate());
+	}
+
+	void Update()
+	{
+
+	}
+
+	public void updateImage()
+	{
+		mainImage.sprite = tutorialImages[count];
+	}
+
+	private IEnumerator WaitForUpdate()
+	{
+		yield return new WaitForSeconds(waitRange * 0.5f);
+
+		iTween.ScaleTo(gameObject, defaultSize, easeTime);
+
+		yield return new WaitForSeconds(waitRange);
+
+		iTween.ScaleTo(gameObject, Vector3.zero, easeTime);
+
+		yield return new WaitForSeconds(easeTime);
+
+		++count;
+
+		if (count < tutorialImages.Length)
+		{
+			updateImage();
+
+			StartCoroutine(WaitForUpdate());
+		}
+	}
+}

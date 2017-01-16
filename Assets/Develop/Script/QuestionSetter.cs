054dd6bba2a181e9f6100b7f3f0d3033402ce58b
Syuma Toyama <Syuma Toyama>
2017-01-16 17:20:45 +0900

    no message

---
+++ Assets/Develop/Script/QuestionSetter.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuestionSetter : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] pages;
+    private int count = 0;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            pages[count].SetActive(false);
+
+            if (count < 5)
+            {
+                ++count;
+            }
+            else
+            {
+                count = 0;
+            }
+
+            pages[count].SetActive(true);
+        }
+    }
+}
